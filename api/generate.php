1cde69d4b9360906e02c48f2e356d77ad3844c73
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-12-28 08:55:22 +0700

    refactor: split into functions

--- api/generate.php
+++ api/generate.php
@@ -1,59 +1,81 @@
 <?php
 
-$contentType = $_SERVER['CONTENT_TYPE'] ?? '';
+echo handle();
 
-if (str_contains($contentType, 'application/json')) {
-	$data = json_decode(file_get_contents('php://input'), true);
-	$text = $data['text'];
-} else {
-	$text = $_POST['text'];
+function handle(): string 
+{
+	$text = get_input('text');
+	$model_output = call_model($text);
+	return json_encode(to_array($model_output));
 }
 
-if (!isset($text) || empty($text)) {
-	echo "INPUT ERROR: You must specify a text";
-	throw new Exception("You must specify a text");
+function get_input(string $key): string
+{
+	$contentType = $_SERVER['CONTENT_TYPE'] ?? '';
+
+	if (str_contains($contentType, 'application/json')) {
+		$data = json_decode(file_get_contents('php://input'), true);
+		$input = $data[$key];
+	} else {
+		$input = $_POST[$key];
+	}
+
+	if (!isset($input) || empty($input)) {
+		echo "INPUT ERROR: You must specify a $key";
+		throw new Exception("You must specify a $key");
+	}
+
+	return $input;
 }
 
-// appending newline, because it will be passed as stdin, and stdin recognize one line as a string ending with \n
-$text .= "\n";
+function call_model(string $text): string
+{
+	// appending newline, because it will be passed as stdin,
+	// and stdin recognize one line as a string ending with \n
+	$text .= "\n";
 
-// -1 means get as much labels as possible
-// 0.5 means get only labels with confidence score >= 0.5
-$model_exe_path = "fasttext predict /usr/local/share/fasttext-models/gemini-generated-shuffled.bin - -1 0.5";
-$descriptorspec = [
-	0 => ['pipe', 'r'],
-	1 => ['pipe', 'w'],
-	2 => ['pipe', 'w']
-];
+	// -1 means get as much labels as possible
+	// 0.5 means get only labels with confidence score >= 0.5
+	$model_exe_path = "fasttext predict /usr/local/share/fasttext-models/gemini-generated-shuffled.bin - -1 0.5";
+	$descriptorspec = [
+		0 => ['pipe', 'r'],
+		1 => ['pipe', 'w'],
+		2 => ['pipe', 'w']
+	];
 
-$process = proc_open($model_exe_path, $descriptorspec, $pipes);
+	$process = proc_open($model_exe_path, $descriptorspec, $pipes);
 
-fwrite($pipes[0], $text);
-fclose($pipes[0]);
+	fwrite($pipes[0], $text);
+	fclose($pipes[0]);
 
-$output = stream_get_contents($pipes[1]);
-fclose($pipes[1]);
+	$output = stream_get_contents($pipes[1]);
+	fclose($pipes[1]);
 
-$stderr = stream_get_contents($pipes[2]);
-fclose($pipes[2]);
+	$stderr = stream_get_contents($pipes[2]);
+	fclose($pipes[2]);
 
-$exitcode = proc_close($process);
+	$exitcode = proc_close($process);
 
-if ($exitcode !== 0) {
-	echo "the machine learning model failed $exitcode";
-	throw new RuntimeException("the machine learning model failed $exitcode : " . trim($stderr));
+	if ($exitcode !== 0) {
+		echo "the machine learning model failed $exitcode";
+		throw new RuntimeException("the machine learning model failed $exitcode : " . trim($stderr));
+	}
+
+	// if there's no result, fasttext will return "\n", with "" NOT '\n'
+	if ($output === "\n") {
+		// TODO: save the $text for labelling later
+		// and also report to some slack channel
+
+		return "other";
+	}
+
+	// we also do cleaning here because it's model-specific
+	$clean_output = trim(str_replace('__label__', '', $output));
+	return $clean_output;
 }
 
-// if there's no result, it will return "\n", NOT '\n'
-if ($output === "\n") {
-	// TODO: save the $text for labelling later
-	// and also report to some slack channel
-
-	echo json_encode(["other"]);
-	exit();
+function to_array(string $data): array
+{
+	return explode(' ', $data);
 }
 
-$clean_output = str_replace("\n", '', str_replace('__label__', '', $output));
-$result = explode(' ', $clean_output);
-
-echo json_encode($result);
